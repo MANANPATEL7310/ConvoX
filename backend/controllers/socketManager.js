46ba26465b74947a725dc9e24664b9bad4631ac5
MANANPATEL7310 <mananpatel7310@gmail.com>
2026-02-16 22:10:11 +0530

    separate some portion of code for better readability.

--- backend/controllers/socketManager.js
+++ backend/controllers/socketManager.js
@@ -4,6 +4,16 @@ import {Server} from "socket.io"
 export const connectToSocket=(server)=>{
   const io=new Server(server);
 
+
+ io.on('connection',(socket)=>{
+  console.log("user is connected.");
+
+  socket.on('disconnect',()=>{
+    console.log("user is disconnected.");
+  })
+ })
+
+
   return io;
 }
 
