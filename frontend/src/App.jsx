de3e1ac1e73a90d8b4e2c8f6a338135fc1f078df
MANANPATEL7310 <mananpatel7310@gmail.com>
2026-02-20 16:52:13 +0530

    add routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,33 @@
 import './App.css'
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import Authentication from './pages/Authentication';
+import { AuthProvider } from './contexts/AuthContext';
+import HomeComponent from './pages/Home';
+import History from './pages/History';
+import VideoMeetComponent from './pages/VideoMeet';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
-  
-
   return (
-    <>
-     
-    </>
+    <div className="App">
+       <Router>
+          <AuthProvider>
+              <Routes>
+                  <Route path='/' element={<LandingPage />} />
+                  <Route path='/auth' element={<Authentication />} />
+                  <Route path='*' element={<LandingPage />} />
+
+                  {/* Protected Routes */}
+                  <Route element={<PrivateRoute />}>
+                      <Route path='/home' element={<HomeComponent />} /> 
+                      <Route path='/history' element={<History />} />
+                      <Route path='/:url' element={<VideoMeetComponent />} />
+                  </Route>
+              </Routes>
+          </AuthProvider>
+       </Router>
+    </div>
   )
 }
 
