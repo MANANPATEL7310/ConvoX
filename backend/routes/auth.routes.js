0b6163be1bd576cff3bb13b951b6579218e2a8f4
MANANPATEL7310 <mananpatel7310@gmail.com>
2026-02-22 19:13:36 +0530

    set route and set SecretToken

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,14 @@
+import { Signup, Login, addToHistory, getUserHistory, Logout } from "../controllers/authController.js";
+import { verifyToken, userVerification } from "../middleware/authMiddleware.js";
+import express from "express";
+
+const router = express.Router();
+
+router.post("/signup", Signup);
+router.post("/login", Login);
+router.post("/verify", userVerification);
+router.post("/logout", Logout);
+router.post("/add_history", verifyToken, addToHistory);
+router.get("/get_history", verifyToken, getUserHistory);
+
+export default router;
