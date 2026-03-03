4ca0f229f88cb34934ba6829b4ee2b6beedaa0fe
MANANPATEL7310 <mananpatel7310@gmail.com>
2026-03-03 18:47:31 +0530

    fix: set more number of request allowed for protection perspective

--- backend/index.js
+++ backend/index.js
@@ -48,7 +48,7 @@ app.use(compression());
 // Rate limiting to prevent abuse
 const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per windowMs
+    max: 2000, // Limit each IP to 2000 requests per windowMs
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     message: { success: false, message: "Too many requests from this IP, please try again in 15 minutes." }
